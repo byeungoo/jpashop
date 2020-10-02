448e843fffe8dcae873fca4faeb67a86c4e27e7d
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-02 12:53:36 +0900

    회원가입 예제 테스트 코드 작성

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -19,7 +22,11 @@ class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
+    //@Rollback(false) //테스트코드에 @Transactional이 있으면 기본적으로 rollback하기 때문에 insert 쿼리문이 콘솔에 안보임.
     public void 회원가입() throws Exception{
         //given
         Member member = new Member();
@@ -29,16 +36,28 @@ class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         //then
+        em.flush(); //쿼리 flush
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
     @Test
     public void 중복_회원_예외() throws Exception{
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);});
 
         //then
+        assertEquals("이미 존재하는 회원입니다.", e.getMessage());
+        //fail("예외가 발생해야 합니다."); //밑으로 로직이 나오면 실패처리. Assert의 fail 사용
     }
 
 }
