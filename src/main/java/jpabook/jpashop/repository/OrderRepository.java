8bf5fa2ca5727fe8ec1bf6f58a939c5e72d49db4
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-10 21:59:04 +0900

    주문 조회 기능 개발 (동적 쿼리)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.repository;
 
+
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +24,46 @@ public class OrderRepository {
 
     public Order findOne(Long id){
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSerarch orderSerarch){
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status " +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSerarch.getOrderStatus())
+                .setParameter("name", orderSerarch.getMemberName())
+                .setMaxResults(1000) //최대 1000건
+                .getResultList()
+        ;
+    }
+
+    /*
+     * JPA Criteria
+     * JPA 표준 스펙에 있지만, 실무에서 안씀.
+     */
+    public List<Order> findAllByCriteria(OrderSerarch orderSerarch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색색
+       if(orderSerarch.getOrderStatus() != null){
+            Predicate status = cb.equal(o.get("status"), orderSerarch.getOrderStatus());
+            criteria.add(status);
+        }
+
+       //회원 이름 거색
+        if(StringUtils.hasText(orderSerarch.getMemberName())){
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSerarch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
     }
 
 }
---
+++ src/main/java/jpabook/jpashop/repository/OrderSerarch.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSerarch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+}
