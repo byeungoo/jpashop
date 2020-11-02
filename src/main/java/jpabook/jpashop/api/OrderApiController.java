9ee13a9f972099fde4c1e79acc94fb93b47cfc62
ByeonGooHoon <dksekfldks65@naver.com>
2020-11-02 22:10:29 +0900

    컬렉션 최적화 예제1 (엔티티 직접 노출) 작성

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSerarch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+ *  컬렉션 조회 최적화 예제
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /*
+     * 엔티티 직접 노출
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSerarch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName()); //강제 초기화
+        }
+        return all;
+    }
+
+}
